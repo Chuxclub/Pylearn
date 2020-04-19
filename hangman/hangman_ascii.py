cc0572d7386d4124c21e47b1595bd30105a05d1e
Florian <florian-1992@hotmail.fr>
2020-04-19 15:48:52 +0200

    Added hangman ascii arts module

---
+++ hangman/hangman_ascii.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+def hangman_state1():
+    print("============ ")
+
+
+def hangman_state2():
+    print("   |         ")
+    print("   |         ")
+    print("   |         ")
+    print("============ ")
+
+
+def hangman_state3():
+    print("   ________  ")
+    print("   |         ")
+    print("   |         ")
+    print("   |         ")
+    print("   |         ")
+    print("   |         ")
+    print("   |         ")
+    print("============ ")
+
+
+def hangman_state4():
+    print("   ________  ")
+    print("   |/        ")
+    print("   |         ")
+    print("   |         ")
+    print("   |         ")
+    print("   |         ")
+    print("   |         ")
+    print("============ ")
+
+
+def hangman_state5():
+    print("   ________    ")
+    print("   |/     |    ")
+    print("   |           ")
+    print("   |           ")
+    print("   |           ")
+    print("   |           ")
+    print("   |           ")
+    print("============   ")
+
+
+def hangman_state6():
+    print("   ________  ")
+    print("   |/     |  ")
+    print("   |      o  ")
+    print("   |         ")
+    print("   |         ")
+    print("   |         ")
+    print("   |         ")
+    print("============ ")
+
+
+def hangman_state7():
+    print("   ________   ")
+    print("   |/     |   ")
+    print("   |      o   ")
+    print("   |     /|\\ ")
+    print("   |          ")
+    print("   |          ")
+    print("   |          ")
+    print("============  ")
+
+
+def hangman_state8():
+    print("   ________   ")
+    print("   |/     |   ")
+    print("   |      o   ")
+    print("   |     /|\\ ")
+    print("   |     / \\ ")
+    print("   |          ")
+    print("   |          ")
+    print("============  ")
+
+
+def hangman_states(case):
+    switcher = {
+        1: hangman_state1,
+        2: hangman_state2,
+        3: hangman_state3,
+        4: hangman_state4,
+        5: hangman_state5,
+        6: hangman_state6,
+        7: hangman_state7,
+        8: hangman_state8
+    }
+
+    return switcher.get(case, "State doesn't exist")()
+
+
+def hangman_saved():
+    print("\\o/")
+    print(" | ")
+    print("/ \\")
+
+
+hangman_states(7)
